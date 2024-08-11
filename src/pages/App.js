b3c03b576e6f503b6b46509aaf716eb7c42f5dc0
Perdana Main <firman.fp123@gmail.com>
2024-08-12 01:46:42 +0700

    make charge

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,16 +1,29 @@
 import { useEffect, useState } from "react";
-import "../css/style.css";
 import Swal from "sweetalert2";
+import axios from "axios";
+import "../css/style.css";
+
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
   const [table, setTable] = useState("Dine In");
   const [cart, setCart] = useState([]);
   const [totalCart, setTotalCart] = useState(0);
+  const [cash, setCash] = useState(0);
+  const [customer, setCustomer] = useState("");
+
+  const [loading, setLoading] = useState(true);
+  const [onSubmit, setOnSubmit] = useState(false);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const handleAddToCart = (product) => {
-    const newCart = cart.filter((item) => item.id !== product.id);
+    const newCart = cart.filter((item) => item.productId !== product.productId);
     if (newCart.length === cart.length) {
       setCart([...cart, { ...product, qty: 1 }]);
     } else {
@@ -40,60 +53,56 @@ const App = () => {
     });
   };
 
-  useEffect(() => {
-    const data = [
-      {
-        id: 1,
-        name: "Product 1",
-        price: 10000,
-        stock: 10,
-      },
-      {
-        id: 2,
-        name: "Product 2",
-        price: 20000,
-        stock: 20,
-      },
-      {
-        id: 3,
-        name: "Product 3",
-        price: 30000,
-        stock: 30,
-      },
-      {
-        id: 4,
-        name: "Product 4",
-        price: 40000,
-        stock: 40,
-      },
-      {
-        id: 5,
-        name: "Product 5",
-        price: 50000,
-        stock: 50,
-      },
-      {
-        id: 6,
-        name: "Product 6",
-        price: 60000,
-        stock: 60,
-      },
-      {
-        id: 7,
-        name: "Product 7",
-        price: 70000,
-        stock: 70,
-      },
-      {
-        id: 8,
-        name: "Product 8",
-        price: 80000,
-        stock: 80,
-      },
-    ];
+  const handleCharge = async (e) => {
+    e.preventDefault();
 
-    setProducts(data);
-    setLoading(false);
+    setOnSubmit(true);
+
+    const data = {
+      total: totalCart,
+      cash: cash,
+      customer: customer,
+      table: table,
+      carts: cart,
+    };
+
+    axios
+      .post("http://localhost:5000/api/v1/orders/", data)
+      .then((response) => {
+        console.log(response);
+        Swal.fire({
+          title: "Success",
+          text: response.data.message,
+          icon: "success",
+        });
+        setOnSubmit(false);
+        setShow(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        Swal.fire({
+          title: "Error",
+          text: error.response.data.message,
+          icon: "error",
+        });
+        setOnSubmit(false);
+      });
+  };
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/v1/products"
+        );
+
+        setProducts(response.data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProducts();
   }, []);
 
   return (
@@ -120,7 +129,7 @@ const App = () => {
                       />
                       <div className="container my-1 mt-2">
                         <h5 className="card-title text-center">
-                          {product.name}
+                          {product.name + " - " + product.price}
                         </h5>
                       </div>
                     </div>
@@ -243,12 +252,69 @@ const App = () => {
                 padding: "10px 51px 10px 51px",
                 backgroundColor: "#485ca1",
               }}
+              onClick={handleShow}
             >
-              Charge
+              Charge Rp. {totalCart}
             </button>
           </div>
         </div>
       </div>
+      {/* modal charge */}
+      <Modal show={show} onHide={handleClose}>
+        <form onSubmit={handleCharge}>
+          <Modal.Header closeButton>
+            <Modal.Title>Charge Information</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Total Charge
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                value={totalCart}
+                disabled
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Total Cash
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                value={cash}
+                onChange={(e) => {
+                  setCash(e.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Nama Customer
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                value={customer}
+                onChange={(e) => {
+                  setCustomer(e.target.value);
+                }}
+              />
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <button className="btn btn-primary" type="submit">
+              {onSubmit ? "Loading..." : "Save"}
+            </button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+      ;
     </div>
   );
 };
